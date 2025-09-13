24ca19599b09b75dff74bfb918e446efa3a99fa7
soliwalker <chrisisfior@gmail.com>
2025-09-13 13:20:28 +0200

    refactor(worker): use a map for routing
    Refactor the workers fetch handler to use a map for routing instead of a long chain of if statements. This makes the code cleaner, more scalable, and easier to maintain.
    
    The new logic also handles stripping the .html extension from the URL path, allowing users to access pages with or without the extension.

--- src/index.js
+++ src/index.js
@@ -4,33 +4,29 @@ import termsHtml from './../terms.html';
 import disclaimerHtml from './../disclaimer.html';
 import pdfHtml from './../pdf.html';
 
+const pages = {
+  '/': indexHtml,
+  '/index': indexHtml,
+  '/privacy': privacyHtml,
+  '/terms': termsHtml,
+  '/disclaimer': disclaimerHtml,
+  '/pdf': pdfHtml,
+};
+
 export default {
   async fetch(request) {
     const url = new URL(request.url);
     let path = url.pathname;
 
-    if (path === '/' || path === '/index.html') {
-      return new Response(indexHtml, {
-        headers: { 'Content-Type': 'text/html' },
-      });
+    // remove .html extension
+    if (path.endsWith('.html')) {
+      path = path.slice(0, -5);
     }
-    if (path === '/privacy' || path === '/privacy.html') {
-      return new Response(privacyHtml, {
-        headers: { 'Content-Type': 'text/html' },
-      });
-    }
-    if (path === '/terms' || path === '/terms.html') {
-      return new Response(termsHtml, {
-        headers: { 'Content-Type': 'text/html' },
-      });
-    }
-    if (path === '/disclaimer' || path === '/disclaimer.html') {
-      return new Response(disclaimerHtml, {
-        headers: { 'Content-Type': 'text/html' },
-      });
-    }
-    if (path === '/pdf' || path === '/pdf.html') {
-      return new Response(pdfHtml, {
+
+    const html = pages[path];
+
+    if (html) {
+      return new Response(html, {
         headers: { 'Content-Type': 'text/html' },
       });
     }
